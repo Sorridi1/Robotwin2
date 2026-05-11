5e3205513acbcbdf5faac549daa8866887e8d945
Sorridi1 <2453002645@qq.com>
2026-05-11 15:07:02 +0800

    Update RoboTwin,including task_config, training_epoch

--- RoboTwin/policy/ManiFlow/train_eval_robotwin2.sh
+++ RoboTwin/policy/ManiFlow/train_eval_robotwin2.sh
@@ -6,7 +6,7 @@
 # bash train_eval_robotwin2.sh lift_pot maniflow_image_transformer_policy_robotwin2 demo_randomized 50 1112 0 0
 
 
-train=false
+train=true
 eval=true
 train_task_config=${3} # setting for training, demo_clean or demo_randomized, add here for clarity
 eval_task_config=${train_task_config} # use the same config for evaluation unless you override it manually
